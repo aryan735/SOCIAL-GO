c1c87da101efc51a9d83cbf14526e959704798f8
aryan735 <aryan735@users.noreply.github.com>
2026-07-07 02:07:15 +0530

    Update users store: internal/store/users.go

--- internal/store/users.go
+++ internal/store/users.go
@@ -3,12 +3,25 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
 )
 
-type UsersStore struct {
+type User struct {
+	ID        int64     `json:"ID"`
+	UserName  string    `json:"user_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
+type UserStore struct {
 	db *sql.DB
 }
 
-func(u *UsersStore) Create(ctx context.Context) error{
+func (u *UserStore) Create(ctx context.Context, user *User) error {
+	query := `INSERT INTO USERS (USERNAME, EMAIL, PASSWORD) VALUES($1, $2, $3) RETURNING id, created_at`
+	err := u.db.QueryRowContext(ctx, query, user.UserName, user.Email, user.Password).Scan(&user.ID, &user.CreatedAt)
+	if err != nil {
+		return err
+	}
 	return nil
 }
