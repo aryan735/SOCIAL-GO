2f4b98ec05c42b3555dec7d84a973a29afde37d2
aryan735 <aryan735@users.noreply.github.com>
2026-07-07 02:07:09 +0530

    Update API main: cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/aryan735/SOCIAL-GO/internal/db"
 	"github.com/aryan735/SOCIAL-GO/internal/env"
 	"github.com/aryan735/SOCIAL-GO/internal/store"
 	"github.com/joho/godotenv"
@@ -13,10 +14,28 @@ func main() {
 		log.Println("No .env file found, using system environment variables")
 
 	}
-	cfg := config{addr: env.GetString("ADDR", ":8080")}
-	store:= store.NewStorage(nil)
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN-CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+	}
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Print("db connected")
+	store := store.NewStorage(db)
 	app := application{config: cfg, store: store}
-
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
